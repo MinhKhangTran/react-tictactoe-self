ca844a079b2671e82b1f6dbab76d4d9264c6ba42
MinhKhangTran <67480568+MinhKhangTran@users.noreply.github.com>
2020-11-03 15:19:40 +0100

    adding timetravel (optional)

--- src/App.js
+++ src/App.js
@@ -3,39 +3,58 @@ import Board from "./Board";
 import { winningPerson } from "./calculateWinner";
 
 export default function App() {
-  const [board, setBoard] = React.useState(Array(9).fill(null));
+  // array of array to call the array number
+  const [history, setHistory] = React.useState([Array(9).fill(null)]);
   const [isX, setIsX] = React.useState(true);
-  const winner = winningPerson(board);
+  const [stepNumber, setStepnumber] = React.useState(0);
+  const winner = winningPerson(history[stepNumber]);
 
   const handleOnClick = (e) => {
-    const boardCopy = [...board];
+    // einzelne Array moves speichern
+    const timeInHistory = history.slice(0, stepNumber + 1);
+    // der jetzige dann in die array of array pushen mit set History
+    const current = timeInHistory[stepNumber];
+    const boardCopy = [...current];
     // wenn es einen gewinner gibt oder auf dem Feld schon ein symbol ist. returne
     if (winner || boardCopy[e]) return;
     // ansonsten lege auf dem Feld ein symbol
     boardCopy[e] = isX ? "💩" : "🍜";
     // Das neue Board array anwenden
-    setBoard(boardCopy);
+    setHistory([...timeInHistory, boardCopy]);
+    setStepnumber(timeInHistory.length);
     //spieler wechseln
     setIsX(!isX);
   };
 
+  // time travel
+
+  const jumpTo = (step) => {
+    setStepnumber(step);
+    setIsX(step % 2 === 0);
+  };
+
   return (
     <>
-      <Board board={board} handleOnClick={handleOnClick} />
+      <Board board={history[stepNumber]} handleOnClick={handleOnClick} />
       <div className="text-center mt-8 text-2xl">
         <p>
           {winner
             ? "Winner is:" + winner
             : "Next Player is:" + (isX ? "💩" : "🍜")}
         </p>
-        <button
-          className="border-indigo-500 rounded border-2 mt-4 hover:bg-indigo-500 hover:text-indigo-100 transition transition-all duration-300 ease-in-out px-3"
-          onClick={() => {
-            setBoard(Array(9).fill(null));
-          }}
-        >
-          Start the Game
-        </button>
+        {history.map((_step, index) => {
+          return (
+            <li key={index}>
+              <button
+                onClick={() => {
+                  jumpTo(index);
+                }}
+              >
+                {index ? `Go to Move #${index}` : "Go to start"}
+              </button>
+            </li>
+          );
+        })}
       </div>
     </>
   );
